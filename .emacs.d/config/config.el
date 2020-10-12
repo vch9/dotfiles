35eb69355181afd53541a7794cdf93a00397dff2
vch9 <valentinchb@gmail.com>
2020-10-12 12:10:55 +0200

    Org mode todod list

--- .emacs.d/config/config.el
+++ .emacs.d/config/config.el
@@ -24,3 +24,29 @@
 (autoload 'whitespace-mode
   "whitespace"
   "Toggle whitespace visualization." t)
+
+;; Org mode
+;; set key for agenda
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+;;file to save todo items
+(setq org-agenda-files (quote ("/home/valentin/todo.org")))
+
+;;set priority range from A to C with default A
+(setq org-highest-priority ?A)
+(setq org-lowest-priority ?C)
+(setq org-default-priority ?A)
+
+;;set colours for priorities
+(setq org-priority-faces '((?A . (:foreground "#F0DFAF" :weight bold))
+                           (?B . (:foreground "LightSteelBlue"))
+                           (?C . (:foreground "OliveDrab"))))
+
+;;open agenda in current window
+(setq org-agenda-window-setup (quote current-window))
+
+;;capture todo items using C-c c t
+(define-key global-map (kbd "C-c c") 'org-capture)
+(setq org-capture-templates
+      '(("t" "todo" entry (file+headline "/home/valentin/todo.org" "Tasks")
+         "* TODO [#A] %?")))
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -28,3 +28,6 @@
 
 (use-package whitespace
   :ensure t)
+
+(use-package org
+  :ensure t)
